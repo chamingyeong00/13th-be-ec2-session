3a5966dfd066731d7280d4960ec9fc615f0931a7
chamingyeong <mingyeong1104@naver.com>
2025-06-02 18:20:22 +0900

    modified Swagger

---
+++ src/main/java/project/ec2session/domain/user/api/UserApi.java
@@ -0,0 +1,87 @@
+package project.ec2session.domain.user.api;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import project.ec2session.common.auth.CustomUserDetails;
+import project.ec2session.domain.user.dto.UserReq;
+
+@Tag(name = "[유저 관련 API]", description = "유저 관련 API")
+public interface UserApi {
+    @Operation(summary = "로그인한 유저 정보 조회", description = "로그인한 유저 정보 반환 시도 (인증된 유저만 접근 가능)")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인한 유저 정보 반환 성공",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                    	"userId": 1,
+                                                                 "username": "test",
+                                                                 "nickname": "테스터"
+                                    }
+                                    """)
+                    })),
+            @ApiResponse(responseCode = "404", description = "정보 조회 실패",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                        "status" : 404,
+                                        "message" : "존재하지 않는 회원입니다."
+                                    }
+                                    """)
+                    }))
+    })
+    ResponseEntity<?> getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails);
+
+    @Operation(summary = "모든 회원 목록 조회", description = "모든 회원의 정보 반환 시도 (인증된 유저만 접근 가능)")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "모든 회원의 정보 반환 성공",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    [
+                                              {
+                                                  "userId": 1,
+                                                  "username": "test1",
+                                                  "nickname": "테스터1"
+                                              },
+                                              {
+                                                  "userId": 2,
+                                                  "username": "test2",
+                                                  "nickname": "테스터2"
+                                              }
+                                    ]
+                                    """)
+                    }))
+    })
+    ResponseEntity<?> getAllUser();
+
+    @Operation (summary = "로그인한 유저 정보 수정", description = "로그인한 유저 정보 수정 시도 (인증된 유저만 접근 가능)")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인한 유저 정보 수정 성공",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+						  			    "요청 성공"
+							  		""")
+                    })),
+            @ApiResponse(responseCode = "404", description = "로그인한 정보 수정 실패",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                        "status": 404,
+                                        "message": "존재하지 않는 회원입니다."
+                                    }
+                                    """)
+                    }))
+    })
+    ResponseEntity<?> updateUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                     @RequestBody @Valid UserReq.UpdateInfo request);
+}
+
--- src/main/java/project/ec2session/domain/user/controller/AuthController.java
+++ src/main/java/project/ec2session/domain/user/controller/AuthController.java
@@ -1,11 +1,5 @@
 package project.ec2session.domain.user.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.ec2session.domain.user.api.AuthApi;
 import project.ec2session.domain.user.dto.TokenDto;
 import project.ec2session.domain.user.dto.UserReq;
 import project.ec2session.domain.user.service.AuthService;
@@ -23,25 +18,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-@Tag(name = "[인증 관련 API]", description = "인증 관련 API")
-public class AuthController{
+public class AuthController implements AuthApi {
     private final AuthService authService;
 
-    @Operation(summary = "로그인", description = "로그인 시도")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "로그인 성공",
-                    content = @Content(mediaType = "application/json", examples = {
-                            @ExampleObject(value = """
-						  			{
-							  			"accessToken" : "<accessToken>",
-							  			"refreshToken" : "<refreshToken>"
-							  		}
-							  		""")
-                    })),
-            @ApiResponse(responseCode = "400", description = "로그인 실패")
-    })
-
-
+    @Override
     @PostMapping("/sign-in")
     public ResponseEntity<?> signIn(
             @RequestBody @Valid UserReq.SignInDto request
@@ -51,19 +31,6 @@ public class AuthController{
         return ResponseEntity.ok().body(response);
     }
 
-    @Operation(summary = "회원가입", description = "회원가입 시도")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "회원가입 성공",
-                    content = @Content(mediaType = "application/json", examples = {
-                            @ExampleObject(value = """
-						  			{
-							  			"accessToken" : "<accessToken>",
-							  			"refreshToken" : "<refreshToken>"
-							  		}
-							  		""")
-                    })),
-            @ApiResponse(responseCode = "400", description = "회원가입 실패")
-    })
 
     @PostMapping("/sign-up")
     public ResponseEntity<?> signUp(@RequestBody @Valid UserReq.SignUpDto request) {
