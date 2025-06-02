2893bdd73a3fad21e423e9a7e26daaf788be8c13
chamingyeong <mingyeong1104@naver.com>
2025-06-02 19:07:39 +0900

    add implements

--- src/main/java/project/ec2session/domain/user/api/UserApi.java
+++ src/main/java/project/ec2session/domain/user/api/UserApi.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import project.ec2session.common.auth.CustomUserDetails;
 import project.ec2session.domain.user.dto.UserReq;
